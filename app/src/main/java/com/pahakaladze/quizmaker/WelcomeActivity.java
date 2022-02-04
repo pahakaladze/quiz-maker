737ac3a85785de56b6441c15d14745dd8026e148
Pavlo Ihonin <pahakaladze@gmail.com>
2022-02-04 01:38:28 +0100

    added WelcomeActivity.java;

--- app/src/main/java/com/pahakaladze/quizmaker/WelcomeActivity.java
+++ app/src/main/java/com/pahakaladze/quizmaker/WelcomeActivity.java
@@ -2,7 +2,9 @@ package com.pahakaladze.quizmaker;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class WelcomeActivity extends AppCompatActivity {
 
@@ -10,5 +12,15 @@ public class WelcomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
+        getSupportActionBar().hide();
+    }
+
+    public void startQuiz(View view){
+
+    }
+
+    public void startQuizMaker(View view){
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
     }
 }
